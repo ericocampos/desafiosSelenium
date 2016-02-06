278459c8fb808b9ad2c06fc009a08f52bbdfae99
ericocampos <ericocamposlira@gmail.com>
2016-02-05 22:33:55 -0300

    Primeiro Comit

--- src/desafiosSelenium/desafio1.java
+++ src/desafiosSelenium/desafio1.java
@@ -9,10 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.thoughtworks.selenium.Wait;
